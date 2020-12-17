3fd1234a1629ebbc61f6d79065080b3541e720c2
bernarducs <bernarducs@gmail.com>
2020-12-16 23:20:44 -0300

    album of the day

--- dao/dao_media.py
+++ dao/dao_media.py
@@ -23,7 +23,10 @@ def album_of_day():
     today = dt.strftime(dt.today(), "%m-%d")
     df_album = df.query("month_day == @today").head(1)
 
-    album = df_album.album.iloc[0]
-    path = df_album.path.iloc[0]
-
-    return album, path
+    if df_album.shape[0] != 0:
+        album = df_album.album.iloc[0]
+        path = df_album.path.iloc[0]
+        return album, path
+    else:
+        album, path = None, None
+        return album, path
